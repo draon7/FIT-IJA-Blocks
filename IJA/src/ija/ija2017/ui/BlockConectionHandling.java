c7ed613adea984be1af3b11cf9be3849c6e0d5a2
Tomáš Deingruber <Deingurber.tomas@gmail.com>
2018-05-06 14:09:41 +0200

    adding newSchemes

--- IJA/src/ija/ija2017/ui/BlockConectionHandling.java
+++ IJA/src/ija/ija2017/ui/BlockConectionHandling.java
@@ -4,6 +4,7 @@ import ija.ija2017.port.AbstractPort;
 import ija.ija2017.port.InputPort;
 import ija.ija2017.port.OutputPort;
 import ija.ija2017.scheme.Scheme;
+import javafx.scene.layout.AnchorPane;
 
 import java.util.ArrayList;
 
@@ -12,10 +13,33 @@ public class BlockConectionHandling {
     public static BlockConectionHandling getSelf(){
         return self;
     }
-    private ArrayList<Scheme> schemes = new ArrayList<Scheme>();
+    private static Scheme activeScheme;
+    private static ArrayList<Scheme> schemes = new ArrayList<Scheme>();
+
+    public static ArrayList<Scheme> getSchemes() {return schemes;}
+    public static void setSchemes(ArrayList<Scheme> schemes) {BlockConectionHandling.schemes = schemes;}
+
+
+    private static AnchorPane mainView;
+
+    public static void setMainView(AnchorPane mainView){BlockConectionHandling.mainView = mainView;}
+    public static AnchorPane getMainView() {return mainView;}
 
     private static InputPort inputPort;
     private static OutputPort outputPort;
+
+    public static void addScheme(String id){
+        getSchemes().add(new Scheme(id));
+        changeScheme(id);
+    }
+    public static void changeScheme(String id){
+        for (Scheme scheme : getSchemes()) {
+            if(scheme.getName() == id){
+                activeScheme = scheme;
+                return;
+            }
+        }
+    }
 
     public static boolean setInput(InputPort port){inputPort = port; if(tryConnect()){return true;} return false;}
     public static InputPort getInput(){return inputPort;}
@@ -23,6 +47,7 @@ public class BlockConectionHandling {
     public static boolean setOutput(OutputPort port){outputPort = port; if(tryConnect()){return true;} return false;}
     public static OutputPort getOutput(){return outputPort;}
     public static void removeOutput(){outputPort = null;}
+
     private static boolean tryConnect(){
         System.out.println("In : Out - " + inputPort + " : " + outputPort);
         if(inputPort != null && outputPort != null){
--- IJA/src/ija/ija2017/ui/MainWindowController.java
+++ IJA/src/ija/ija2017/ui/MainWindowController.java
@@ -1,12 +1,23 @@
 package ija.ija2017.ui;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
+
 public class MainWindowController {
 
+    public MainWindowController(){
+        BlockConectionHandling.setMainView(mainViewPane);
+    }
+
     @FXML private AnchorPane mainViewPane;
+    @FXML private TabPane tabPane;
+    @FXML private Tab addScheme;
 
     @FXML
     protected void createBlockAttack(MouseEvent e){
@@ -27,5 +38,27 @@ public class MainWindowController {
     @FXML
     protected void createBlockWeaponUpgrade(MouseEvent e){
         BlockWeaponUpgradeUI blockWeaponUpgrade = new BlockWeaponUpgradeUI(mainViewPane);
+    }
+    @FXML
+    protected void changeScheme(){
+        int index = 0;
+        if(tabPane.getSelectionModel().getSelectedItem().isSelected()){
+            index = tabPane.getSelectionModel().getSelectedIndex();
+            BlockConectionHandling.changeScheme(tabPane.getSelectionModel().getSelectedItem().getText());
+        }
+        if(tabPane.getTabs().indexOf(addScheme) == index){
+            index += 1;
+            Tab tab = new Tab("newScheme" + index);
+            tab.setOnSelectionChanged(new EventHandler<Event>() {
+                @Override
+                public void handle(Event event) {
+                    changeScheme();
+                }
+            });
+            tabPane.getTabs().add(tab);
+            tabPane.getTabs().remove(addScheme);
+            tabPane.getTabs().add(addScheme);
+            BlockConectionHandling.addScheme(tab.getText());
+        }
     }
 }
